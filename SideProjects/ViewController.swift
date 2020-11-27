f6b3a68a6d36a68e7d4c00605d2c598193277122
thond02 <thond02@ominext.com>
2020-11-27 16:22:13 +0700

    Initial Commit

---
+++ SideProjects/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SideProjects
+//
+//  Created by Nguyen Duc Tho on 11/27/20.
+//  Copyright © 2020 Nguyen Duc Tho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
